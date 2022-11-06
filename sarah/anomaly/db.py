15fb4130aadaa2647153392b2ed2a3e46d957900
UsernameForGerman <37830791+UsernameForGerman@users.noreply.github.com>
2022-11-07 00:07:31 +0300

    fix(anomaly): change butch reading from db
    Co-authored-by: UsernameForGerman <gpolyak@polyak.cc>

--- sarah/anomaly/db.py
+++ sarah/anomaly/db.py
@@ -8,6 +8,8 @@ from sqlalchemy.ext import asyncio as aorm
 from sarah import deps
 
 
+DATABASE_APPLICATIONS_AMOUNT = 500000
+
 # internal
 
 def _make_group_by(
@@ -74,166 +76,181 @@ async def get_restricted_repeated_applications(
 async def get_close_wo_completion_first(
     db: aorm.AsyncSession,
     log: tp.Any,
-    batch_size: int = 10
+    batch_size: int = 100000
 ) -> tp.List[dict]:
-    query = await db.stream(
-        sqlalchemy.text(
-            """
-            SELECT
-                id,
-                "Дата закрытия",
-                "Наименование дефекта",
-                applicant_id,
-                application.application_creation_timestamp as application_creation_timestamp
-            FROM application
-            WHERE "Наименование статуса заявки" IN ('Закрыта', 'Закрыта через МАРМ')
-                AND "Наименование дефекта" NOT IN ('Ввод в эксплуатацию ИПУ воды (замена, демонтаж, пропуск межповерочного интервала)', 'Подача документов о поверке ИПУ
-            воды в электронном виде')
-                AND "Результативность" != 'Выполнено'
-                AND "Вид выполненных работ" != 'Аварийное/плановое отключение' 
-                AND is_anomaly = false
-            LIMIT 100;
-            """
-        ),
-        execution_options={"yield_per": batch_size, "stream_results": True}
-    )
-
-    data = await query.mappings().fetchmany(batch_size)
-    while data:
-        yield(data)
-        data = await query.mappings().fetchmany(batch_size)
+    data = []
+    _data = [0]
+    i = 1
+    while i != int(DATABASE_APPLICATIONS_AMOUNT / batch_size):
+        query = await db.stream(
+            sqlalchemy.text(
+                """
+                SELECT
+                    id,
+                    "Дата закрытия",
+                    "Наименование дефекта",
+                    applicant_id,
+                    application.application_creation_timestamp as application_creation_timestamp
+                FROM application
+                WHERE "Наименование статуса заявки" IN ('Закрыта', 'Закрыта через МАРМ')
+                    AND "Наименование дефекта" NOT IN ('Ввод в эксплуатацию ИПУ воды (замена, демонтаж, пропуск межповерочного интервала)', 'Подача документов о поверке ИПУ
+                воды в электронном виде')
+                    AND "Результативность" != 'Выполнено'
+                    AND "Вид выполненных работ" != 'Аварийное/плановое отключение' 
+                    AND (is_anomaly is false or is_anomaly is null)
+                OFFSET :offset LIMIT :batch_size;
+                """
+            ).bindparams(offset=batch_size*i, batch_size=batch_size),
+            execution_options={"yield_per": batch_size, "stream_results": True}
+        )
+        _data = await query.mappings().fetchmany()
+        data.extend(await query.mappings().fetchmany())
+        i += 1
+    yield data
 
 
 async def get_close_wo_completion_second(
     db: aorm.AsyncSession,
     log: tp.Any,
-    batch_size: int = 10
+    batch_size: int = 100000
 ) -> tp.List[dict]:
-    query = await db.stream(
-        sqlalchemy.text(
-            """
-            SELECT
-                id,
-                "Дата закрытия",
-                "Наименование дефекта",
-                applicant_id,
-                application.application_creation_timestamp as application_creation_timestamp
-            FROM application
-            WHERE "Наименование статуса заявки" IN ('Закрыта', 'Закрыта через МАРМ')
-                AND "Наименование дефекта" NOT IN ('Ввод в эксплуатацию ИПУ воды (замена, демонтаж, пропуск межповерочного интервала)', 'Подача документов о поверке ИПУ
-            воды в электронном виде')
-                AND "Результативность" = 'Выполнено'
-                AND COALESCE(EXTRACT(epoch FROM application.application_closure_timestamp - application.application_creation_timestamp), 0) / 60 > 10
-                AND ("Кол-во возвратов на доработку" IS NULL OR "Кол-во возвратов на доработку" = '0') 
-                AND is_anomaly = false
-            LIMIT 100
-            """
-        ),
-        execution_options={"yield_per": batch_size, "stream_results": True}
-    )
-
-    data = await query.mappings().fetchmany(batch_size)
-    while data:
-        yield(data)
-        data = await query.mappings().fetchmany(batch_size)
+    data = []
+    _data = [0]
+    i = 1
+    while i != int(DATABASE_APPLICATIONS_AMOUNT / batch_size):
+        query = await db.stream(
+            sqlalchemy.text(
+                """
+                SELECT
+                    id,
+                    "Дата закрытия",
+                    "Наименование дефекта",
+                    applicant_id,
+                    application.application_creation_timestamp as application_creation_timestamp
+                FROM application
+                WHERE "Наименование статуса заявки" IN ('Закрыта', 'Закрыта через МАРМ')
+                    AND "Наименование дефекта" NOT IN ('Ввод в эксплуатацию ИПУ воды (замена, демонтаж, пропуск межповерочного интервала)', 'Подача документов о поверке ИПУ
+                воды в электронном виде')
+                    AND "Результативность" = 'Выполнено'
+                    AND COALESCE(EXTRACT(epoch FROM application.application_closure_timestamp - application.application_creation_timestamp), 0) / 60 > 10
+                    AND ("Кол-во возвратов на доработку" IS NULL OR "Кол-во возвратов на доработку" = '0') 
+                    AND (is_anomaly is false or is_anomaly is null)
+                OFFSET :offset LIMIT :batch_size;
+                """
+            ).bindparams(offset=batch_size*i, batch_size=batch_size),
+            execution_options={"yield_per": batch_size, "stream_results": True}
+        )
+        _data = await query.mappings().fetchmany()
+        data.extend(await query.mappings().fetchmany())
+        i += 1
+    yield data
 
 
 async def get_close_wo_completion_third(
     db: aorm.AsyncSession,
     log: tp.Any,
-    batch_size: int = 10
+    batch_size: int = 100000
 ) -> tp.List[dict]:
-    query = await db.stream(
-        sqlalchemy.text(
-            """
-            SELECT
-                id,
-                "Дата закрытия",
-                "Наименование дефекта",
-                "Идентификатор дефекта",
-                applicant_id,
-                application.application_creation_timestamp as application_creation_timestamp
-            FROM application
-            WHERE "Наименование статуса заявки" IN ('Закрыта', 'Закрыта через МАРМ')
-                AND "Результативность" = 'Выполнено'
-                AND ("Кол-во возвратов на доработку" IS NULL OR "Кол-во возвратов на доработку" = '0')
-                AND COALESCE(EXTRACT(epoch FROM application.application_closure_timestamp - application.application_creation_timestamp), 0) / 60 < 10
-                AND is_anomaly = false
-            LIMIT 100;
-            """
-        ),
-        execution_options={"yield_per": batch_size, "stream_results": True}
-    )
-
-    data = await query.mappings().fetchmany(batch_size)
-    while data:
-        yield(data)
-        data = await query.mappings().fetchmany(batch_size)
+    data = []
+    _data = [0]
+    i = 1
+    while i != int(DATABASE_APPLICATIONS_AMOUNT / batch_size):
+        query = await db.stream(
+            sqlalchemy.text(
+                """
+                SELECT
+                    id,
+                    "Дата закрытия",
+                    "Наименование дефекта",
+                    "Идентификатор дефекта",
+                    applicant_id,
+                    application.application_creation_timestamp as application_creation_timestamp
+                FROM application
+                WHERE "Наименование статуса заявки" IN ('Закрыта', 'Закрыта через МАРМ')
+                    AND "Результативность" = 'Выполнено'
+                    AND ("Кол-во возвратов на доработку" IS NULL OR "Кол-во возвратов на доработку" = '0')
+                    AND COALESCE(EXTRACT(epoch FROM application.application_closure_timestamp - application.application_creation_timestamp), 0) / 60 < 10
+                    AND (is_anomaly is false or is_anomaly is null)
+                OFFSET :offset LIMIT :batch_size;
+                """
+            ).bindparams(offset=batch_size*i, batch_size=batch_size),
+            execution_options={"yield_per": batch_size, "stream_results": True}
+        )
+        _data = await query.mappings().fetchmany()
+        data.extend(await query.mappings().fetchmany())
+        i += 1
+    yield data
 
 
 async def get_close_wo_completion_fourth(
     db: aorm.AsyncSession,
     log: tp.Any,
-    batch_size: int = 10
+    batch_size: int = 100000
 ) -> tp.List[dict]:
-    query = await db.stream(
-        sqlalchemy.text(
-            """
-            SELECT
-                id,
-                "Дата закрытия",
-                "Наименование дефекта",
-                "Идентификатор дефекта",
-                applicant_id,
-                application.application_creation_timestamp as application_creation_timestamp
-            FROM application
-            WHERE "Наименование статуса заявки" IN ('Закрыта', 'Закрыта через МАРМ')
-                AND "Результативность" = 'Выполнено'
-                AND "Кол-во возвратов на доработку" IS NOT NULL AND "Кол-во возвратов на доработку" != '0'
-                AND COALESCE(EXTRACT(epoch FROM application.application_closure_timestamp - application.application_creation_timestamp), 0) / 60 < 10
-                AND is_anomaly = false
-            LIMIT 100;
-            """
-        ),
-        execution_options={"yield_per": batch_size, "stream_results": True}
-    )
-
-    data = await query.mappings().fetchmany(batch_size)
-    while data:
-        yield(data)
-        data = await query.mappings().fetchmany(batch_size)
+    data = []
+    _data = [0]
+    i = 1
+    while i != int(DATABASE_APPLICATIONS_AMOUNT / batch_size):
+        query = await db.stream(
+            sqlalchemy.text(
+                """
+                SELECT
+                    id,
+                    "Дата закрытия",
+                    "Наименование дефекта",
+                    "Идентификатор дефекта",
+                    applicant_id,
+                    application.application_creation_timestamp as application_creation_timestamp
+                FROM application
+                WHERE "Наименование статуса заявки" IN ('Закрыта', 'Закрыта через МАРМ')
+                    AND "Результативность" = 'Выполнено'
+                    AND "Кол-во возвратов на доработку" IS NOT NULL AND "Кол-во возвратов на доработку" != '0'
+                    AND COALESCE(EXTRACT(epoch FROM application.application_closure_timestamp - application.application_creation_timestamp), 0) / 60 < 10
+                    AND (is_anomaly is false or is_anomaly is null)
+                OFFSET :offset LIMIT :batch_size;
+                """
+            ).bindparams(offset=batch_size*i, batch_size=batch_size),
+            execution_options={"yield_per": batch_size, "stream_results": True}
+        )
+        _data = await query.mappings().fetchmany()
+        data.extend(await query.mappings().fetchmany())
+        i += 1
+    yield data
 
 
 async def get_close_wo_completion_fifth(
     db: aorm.AsyncSession,
     log: tp.Any,
-    batch_size: int = 10
+    batch_size: int = 100000
 ) -> tp.List[dict]:
-    query = await db.stream(
-        sqlalchemy.text(
-            """
-            SELECT
-                id,
-                "Дата закрытия",
-                "Наименование дефекта",
-                "Идентификатор дефекта",
-                applicant_id,
-                application.application_creation_timestamp as application_creation_timestamp
-            FROM application
-            WHERE "Наименование статуса заявки" IN ('Закрыта', 'Закрыта через МАРМ')
-                AND "Результативность" = 'Выполнено'
-                AND "Кол-во возвратов на доработку" IS NOT NULL AND "Кол-во возвратов на доработку" != '0'
-                AND is_anomaly = false
-            LIMIT 100;
-            """
-        ),
-        execution_options={"yield_per": batch_size, "stream_results": True}
-    )
-
-    data = await query.mappings().fetchmany(batch_size)
-    while data:
-        yield(data)
-        data = await query.mappings().fetchmany(batch_size)
+    data = []
+    _data = [0]
+    i = 1
+    while i != int(DATABASE_APPLICATIONS_AMOUNT / batch_size):
+        query = await db.stream(
+            sqlalchemy.text(
+                """
+                SELECT
+                    id,
+                    "Дата закрытия",
+                    "Наименование дефекта",
+                    "Идентификатор дефекта",
+                    applicant_id,
+                    application.application_creation_timestamp as application_creation_timestamp
+                FROM application
+                WHERE "Наименование статуса заявки" IN ('Закрыта', 'Закрыта через МАРМ')
+                    AND "Результативность" = 'Выполнено'
+                    AND "Кол-во возвратов на доработку" IS NOT NULL AND "Кол-во возвратов на доработку" != '0'
+                    AND (is_anomaly is false or is_anomaly is null)
+                OFFSET :offset LIMIT :batch_size;
+                """
+            ).bindparams(offset=batch_size*i, batch_size=batch_size),
+            execution_options={"yield_per": batch_size, "stream_results": True}
+        )
+        _data = await query.mappings().fetchmany()
+        data.extend(await query.mappings().fetchmany())
+        i += 1
+    yield data
 
 
 async def update_applications_is_anomaly(
